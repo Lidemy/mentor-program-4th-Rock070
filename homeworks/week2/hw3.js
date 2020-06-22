a0103188d8049a66eab5bfc04bfde24cc78b6350
王茂己 <rock@wangjiajide-MacBook-Air.local>
2020-06-22 21:53:50 +0800

    week2 自我反省與檢討後修改

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,6 +1,12 @@
 function reverse(str) {
   
-  console.log(str.split("").reverse().join(""))
+
+  var arr=str.split("")
+  var newarr=[];
+  for(var i=arr.length-1 ; i>=0 ; i--){
+    newarr.push(arr[i])
+  }
+  console.log(newarr.join(""))
 }
 
 reverse('hello');
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,7 +1,12 @@
 function join(arr, concatStr) {
     result=""
   for(var i=0;i<arr.length;i++){
+    if(i!=arr.length-1){
       result+=arr[i]+concatStr
+    }else{
+      result+=arr[i]
+    }
+
   }
   return result
 }
@@ -16,3 +21,8 @@ function repeat(str, times) {
 
 console.log(join(["a", 1, "b", 2, "c", 3], ','))
 console.log(repeat('a', 5));
+
+
+console.log(join([1, 2, 3], ''))//，正確回傳值：123
+console.log(join(["a", "b", "c"], "!"))//，正確回傳值：a!b!c
+console.log(join(["aaa", "bb", "c", "dddd"], ',,'))//，正確回傳值：aaa,,bb,,c,,dddd
