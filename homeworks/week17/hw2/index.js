5bc3b14f1b6822dc997702cc61db4d3154957208
王茂己 <rock@wangjiajide-MacBook-Air.local>
2020-11-08 18:44:06 +0800

    week17 作業完成

---
+++ homeworks/week17/hw2/index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const lotteryControl = require('./controls/lottery.js')
+const flash = require('connect-flash');
+const session = require('express-session')
+const cors = require('cors');
+
+
+
+
+
+const app = express()
+const port = 5002
+
+
+function redirectIndex(req, res) {
+    res.redirect('back')
+}
+
+// app set
+app.set('view engine', 'ejs');  
+
+// app use (middleware)
+
+// 1. body-parse
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// 2. express-session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+// 3. connect-flash
+
+app.use(flash());
+
+// 4. 內建提供靜態檔案
+
+app.use('/css', express.static('./views/css'));
+
+// 5. local 儲存全域變數
+
+app.use((req, res, next) => {
+    res.locals.nickname = req.session.nickname || false
+    res.locals.userId = req.session.userId || false
+    res.locals.errMessage = req.flash('errMessage')
+    next()
+})
+// 6. cors() 開放跨來源存取
+
+app.use(cors())
+// app get
+
+// index
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// admin
+app.get('/admin', (req, res) => {
+    res.render('admin')
+})
+
+// prizeList
+app.get('/prizeList', lotteryControl.prizeList)
+
+// lottery
+app.get('/lottery', lotteryControl.lottery)
+app.post('/lottery', lotteryControl.prizeAddPost)
+
+// prize
+
+app.post('/prizeEditPost/:id', lotteryControl.prizeEditPost, redirectIndex)
+app.post('/prizeDeletePost/:id', lotteryControl.prizeDeletePost, redirectIndex)
+app.listen(port, () => {
+    console.log(`success!listen to ${port}`)
+})
+
+
